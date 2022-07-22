7dee25e5d1679f3bbf1ad35708241d57b5e99bdc
JiJiBo <38462693+JiJiBo@users.noreply.github.com>
2022-07-22 09:36:37 +0800

    Initial commit

---
+++ app/src/main/java/com/nas/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nas.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
